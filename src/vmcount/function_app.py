55b0ca9d3b193e9eade30795d51914226d1b1bb9
Pete White <plw@plwhite.org>
2025-09-09 13:32:23 +0100

    Move to use spot instances

--- src/vmcount/function_app.py
+++ src/vmcount/function_app.py
@@ -14,13 +14,19 @@ def vmcount(timer: func.TimerRequest):
     client_id = os.environ["UAMI_CLIENT_ID"]
     subscription_id = os.environ["AZURE_SUBSCRIPTION_ID"]
     rg = os.environ["VMSS_RESOURCE_GROUP"]
-    vmss = os.environ["VMSS_NAME"]
+    vmss_name = os.environ["VMSS_NAME"]
 
     client = ComputeManagementClient(
         credential=ManagedIdentityCredential(client_id=client_id),
         subscription_id=subscription_id
     )
 
-    vmss = client.virtual_machine_scale_sets.get(rg, vmss)
+    vmss = client.virtual_machine_scale_sets.get(rg, vmss_name)
     instance_count = vmss.sku.capacity
     logging.info("METRIC: Current VMSS capacity: %d", instance_count)
+
+    instances = list(client.virtual_machine_scale_set_vms.list(rg, vmss_name))
+    running_instances = [vm for vm in instances if vm.instance_view.statuses and any(
+        s.code == 'PowerState/running' for s in vm.instance_view.statuses)]
+    logging.info("METRIC: Live instance count: %d", len(running_instances))
+    logging.info("METRIC: Total instance count: %d", len(instances))
