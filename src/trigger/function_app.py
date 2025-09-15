f478b03526411e7af1a47d26c807cd7319ea8844
Pete White <plw@plwhite.org>
2025-09-15 11:23:08 +0100

    Remove unused manual HTTP trigger

--- src/trigger/function_app.py
+++ src/trigger/function_app.py
@@ -5,13 +5,6 @@ import logging
 
 app = func.FunctionApp()
 
-@app.function_name(name="ingest_manual")
-@app.route(route="ingest_manual", methods=["POST"], auth_level=func.AuthLevel.ADMIN)
-def ingest_manual(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info("Scaling manually")
-    scale_vmss_to_one()
-    return func.HttpResponse("VMSS scaled to 1", status_code=200)
-
 # Pull the CRON expression from the environment
 TRIGGER_SCHEDULE = os.environ.get("TRIGGER_SCHEDULE", "0 0 9 * * 1")  # default if not set
 
